7460ae9fed27df5815455b56a6c42a9ec0ee5866
FoteinosMerg <foteinosmerg@gmail.com>
2018-10-02 08:36:54 +0200

    minor corrections

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-"use strict"
-import 'dotenv/config'; // import env variables readability; better before all other imports
+"use strict";
+import "dotenv/config"; // import env variables readability; better before all other imports
 
-console.log('\n * Minimal Node.js setup\n');
+console.log("\n * Minimal Node.js setup\n");
 
 const PORT = process.env.PORT || 5000;
