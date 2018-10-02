1df460d0e9c134757cbfd31408392b3ab16388fc
FoteinosMerg <foteinosmerg@gmail.com>
2018-10-02 08:06:43 +0200

    Add deployment options

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@
 import 'dotenv/config'; // import env variables readability; better before all other imports
 
 console.log('\n * Minimal Node.js setup\n');
+
+const PORT = process.env.PORT || 5000;
